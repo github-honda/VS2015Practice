ca65cb8d8d0e6a41b4f7b66a7cd66bed094c16c0
Honda <hondachen@hotmail.com>
2019-03-21 16:53:13 +0800

    Add RSA examples from MS first.

---
+++ SecuritySample/Security1/CProject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security1
+{
+    class CProject
+    {
+        public const string csProjectID = "Security1";
+
+        string[] msaArgs;
+
+        public CProject(string[] args)
+        {
+            msaArgs = args;
+        }
+
+        public static void Create(string[] args)
+        {
+            new CProject(args).Run();
+        }
+
+        public void Run()
+        {
+            // remove comment for test:
+            new SampleRSAMS().Run();
+            //new SampleRSA().Run();
+
+            Console.WriteLine("End.");
+            Console.ReadKey();
+        }
+    }
+}
